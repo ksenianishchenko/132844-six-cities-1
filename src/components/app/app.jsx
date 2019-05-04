b14edc7be4a7eb54ebb18913cebc2de0bf41396a
Kseniia Anishchenko <kseninavoi@gmail.com>
2019-05-04 08:42:31 +0500

    Добавляет proptypes валидацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const App = () => {
+const App = (props) => {
+
+  const {rentPlaces} = props;
   return <React.Fragment>
     <div style={{display: `none`}}>
       <svg xmlns="http://www.w3.org/2000/svg"><symbol id="icon-arrow-select" viewBox="0 0 7 4"><path fillRule="evenodd" clipRule="evenodd" d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z"></path></symbol><symbol id="icon-bookmark" viewBox="0 0 17 18"><path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path></symbol><symbol id="icon-star" viewBox="0 0 13 12"><path fillRule="evenodd" clipRule="evenodd" d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z"></path></symbol></svg>
@@ -120,7 +123,7 @@ const App = () => {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+                    <a href="#">{rentPlaces[0]}</a>
                   </h2>
                   <p className="place-card__type">Apartment</p>
                 </div>
@@ -152,7 +155,7 @@ const App = () => {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Wood and stone place</a>
+                    <a href="#">{rentPlaces[1]}</a>
                   </h2>
                   <p className="place-card__type">Private room</p>
                 </div>
@@ -184,7 +187,7 @@ const App = () => {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Canal View Prinsengracht</a>
+                    <a href="#">{rentPlaces[2]}</a>
                   </h2>
                   <p className="place-card__type">Apartment</p>
                 </div>
@@ -219,7 +222,7 @@ const App = () => {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Nice, cozy, warm big bed apartment</a>
+                    <a href="#">{rentPlaces[3]}</a>
                   </h2>
                   <p className="place-card__type">Apartment</p>
                 </div>
@@ -251,7 +254,7 @@ const App = () => {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Wood and stone place</a>
+                    <a href="#">{rentPlaces[4]}</a>
                   </h2>
                   <p className="place-card__type">Private room</p>
                 </div>
@@ -266,6 +269,10 @@ const App = () => {
 
     </main>
   </ React.Fragment>;
+};
+
+App.propTypes = {
+  rentPlaces: PropTypes.array.isRequired
 };
 
 export default App;
