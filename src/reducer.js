4d3d4be46fcc95a48a2b0d46d85dda68c54367d3
Kseniia Anishchenko <kseninavoi@gmail.com>
2019-05-29 21:18:39 +0500

    Корректирует

--- src/reducer.js
+++ src/reducer.js
@@ -2,13 +2,11 @@ import offers from "./mocks/offers.js";
 
 const initialState = {
   activeCityIndex: 0,
-  activeOffer: {},
   offers
 };
 
 const actionsType = {
-  CHANGE_CITY: `CHANGE_CITY`,
-  GET_ACTIVE_OFFER: `GET_ACTIVE_OFFER`
+  CHANGE_CITY: `CHANGE_CITY`
 };
 
 const ActionCreators = {
@@ -16,10 +14,6 @@ const ActionCreators = {
     type: `CHANGE_CITY`,
     payload: index,
   }),
-  getActiveOffer: (offer) => ({
-    type: `GET_ACTIVE_OFFER`,
-    payload: offer,
-  })
 };
 
 
@@ -27,9 +21,6 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionsType.CHANGE_CITY: return Object.assign({}, state, {
       activeCityIndex: action.payload
-    });
-    case actionsType.GET_ACTIVE_OFFER: return Object.assign({}, state, {
-      activeOffer: action.payload
     });
   }
 
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -11,7 +11,6 @@ it(`Checks action creator`, () => {
 it(`Initial state of reducer`, () => {
   expect(reducer(undefined, {})).toEqual({
     activeCityIndex: 0,
-    activeOffer: {},
     offers
   });
 });
@@ -19,29 +18,12 @@ it(`Initial state of reducer`, () => {
 it(`Checks state of reducer`, () => {
   expect(reducer({
     activeCityIndex: 13,
-    activeOffer: {},
     offers
   }, {
     type: `CHANGE_CITY`,
     payload: 3
   })).toEqual({
     activeCityIndex: 3,
-    activeOffer: {},
-    offers
-  });
-});
-
-it(`Checks active offer`, () => {
-  expect(reducer({
-    activeCityIndex: 13,
-    activeOffer: {},
-    offers
-  }, {
-    type: `GET_ACTIVE_OFFER`,
-    payload: {city: `Paris`}
-  })).toEqual({
-    activeCityIndex: 13,
-    activeOffer: {city: `Paris`},
     offers
   });
 });
