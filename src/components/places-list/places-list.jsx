98c9702c833c2770a17a80170bfa1d6a483b4ec7
Kseniia Anishchenko <kseninavoi@gmail.com>
2019-05-29 21:46:33 +0500

    КОрректирует

--- src/components/places-list/places-list.jsx
+++ src/components/places-list/places-list.jsx
@@ -41,12 +41,6 @@ class PlacesList extends PureComponent {
                 <li className="places__option" tabIndex="0">Price: high to low</li>
                 <li className="places__option" tabIndex="0">Top rated first</li>
               </ul>
-              {/* <select className="places__sorting-type" id="places-sorting">
-                <option className="places__option" value="popular" defaultValue="">Popular</option>
-                <option className="places__option" value="to-high">Price: low to high</option>
-                <option className="places__option" value="to-low">Price: high to low</option>
-                <option className="places__option" value="top-rated">Top rated first</option>
-              </select> */}
             </form>
             <div className="cities__places-list places__list tabs__content">
               {places[activeCityIndex].offers.map((item, index) => {
